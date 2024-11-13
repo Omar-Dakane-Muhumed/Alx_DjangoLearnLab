a3c50dd639a8479dccc0c4761703bf84d59f3a6f
Omar-Dakane-Muhumed <awsaalah451@gmail.com>
2024-11-13 10:48:55 -0500

    my second comit for the permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_view(request, pk):
+    pass
+
+
+["book_list", "raise_exception", "books"]
