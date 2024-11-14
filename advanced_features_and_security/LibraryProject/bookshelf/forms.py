fb5171d4ab3b990ef62d1b5c5a7e0f24093a74cf
Omar-Dakane-Muhumed <awsaalah451@gmail.com>
2024-11-14 05:12:29 -0500

    my first commit for  Implementing Security Best Practices in Django

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,10 @@
+
+""" This code represents an HTML form with security enhancements 
+to protect against Cross-Site Request Forgery (CSRF) attacks in a Django template:
+
+
+<form method="POST">
+    {% csrf_token %}
+    <!-- form fields -->
+</form>
+"""
