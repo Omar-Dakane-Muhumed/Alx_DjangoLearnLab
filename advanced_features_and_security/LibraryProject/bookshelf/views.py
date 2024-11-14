11f58f2a462e08cb3c0cfbd8ddfa584eb13d1395
Omar-Dakane-Muhumed <awsaalah451@gmail.com>
2024-11-14 05:42:28 -0500

    my first commit for  Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -11,8 +11,12 @@ def edit_view(request, pk):
 ["book_list", "raise_exception", "books"]
 
 
-# Instead of this (vulnerable to SQL injection)
-query = "SELECT * FROM books WHERE title = '%s'" % title
 
-# Use ORM like this:
-books = Book.objects.filter(title=title)
+
+# Secure version using Django ORM
+from .models import Book
+
+def search_books(request):
+    title = request.GET.get('title', '')
+    books = Book.objects.filter(title__icontains=title)  # 'icontains' for case-insensitive partial matching
+    return render(request, 'bookshelf/book_list.html', {'books': books})
