d9ac9c3d80b4ca2ba7884a3af448853d89ec9719
Omar Dakane Muhumed <omar@Omar.awsaalah451@gmail.com>
2024-11-22 17:23:25 +0300

    Building my first API endpoint withDjango REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,9 @@
 
 
-
 from rest_framework.generics import ListAPIView
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book  # Import your Book model
+from .serializers import BookSerializer  # Import the serializer
 
 class BookList(ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
+    queryset = Book.objects.all()  # Query all book records
+    serializer_class = BookSerializer  # Use the serializer
