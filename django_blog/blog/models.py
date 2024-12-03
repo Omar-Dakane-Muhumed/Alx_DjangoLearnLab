160cd49679dcba1263c4404ab22275084ed8cd47
Omar Dakane Muhumed <omar@Omar.awsaalah451@gmail.com>
2024-12-03 21:46:58 +0300

    Adde user authentucation system.

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -12,3 +12,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+# Ensure the user can change their email and optionally extend the user model to include more fields like a profile picture or bio.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
