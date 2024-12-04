740dfba113c9fc78762afd751f87077afb7d23b6
Omar Dakane Muhumed <omar@Omar.awsaalah451@gmail.com>
2024-12-04 11:15:14 +0300

    added Blog's user Authentication system.

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -13,3 +13,13 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+# creating user profile
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
