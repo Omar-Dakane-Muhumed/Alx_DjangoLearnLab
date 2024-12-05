1ecf5fb77b2597f9888c91566326a5376a391977
Omar Dakane Muhumed <omar@Omar.awsaalah451@gmail.com>
2024-12-05 15:01:42 +0300

    Creating Blog Post Management Featuresto for the second task.

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -23,3 +23,18 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+# Create the Post Model
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
