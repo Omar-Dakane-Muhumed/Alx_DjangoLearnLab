ab5b3395c4422a3d32e297bec4f13a5c9b4d3f0a
Omar Dakane Muhumed <omar@Omar.awsaalah451@gmail.com>
2024-12-05 21:06:18 +0300

    Adding  Tagging and Search Functionality.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -6,11 +6,14 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['content']
 
-
 from django import forms
+from taggit.forms import TagWidget
 from .models import Post
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']  # Include tags
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(attrs={'class': 'form-control', 'placeholder': 'Add tags'}),
+        }
