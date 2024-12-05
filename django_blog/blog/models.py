d21b5c93df37484f2045f95d99ef28cf1de91db8
Omar Dakane Muhumed <omar@Omar.awsaalah451@gmail.com>
2024-12-05 20:50:28 +0300

    Adding  Tagging and Search Functionality.

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -40,7 +40,7 @@ class Post(models.Model):
 
 
 # Define the Comment Model
-from django.db import models
+"""from django.db import models
 from django.contrib.auth.models import User
 
 class Comment(models.Model):
@@ -52,4 +52,22 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+
+"""
+
+
+
+from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()  # Add tagging functionality
+
+    def __str__(self):
+        return self.title
 
