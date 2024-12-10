051952150ea892ec23ae69fafcee24ac5cf2dbc3
Omar Dakane Muhumed <omar@Omar.awsaalah451@gmail.com>
2024-12-10 22:11:19 +0300

    Project Setup and User Authentication for a Social Media API.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,53 +1,27 @@
-
-
-# Create Serializers
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from .models import CustomUser
+from django.contrib.auth import get_user_model
+
+# Fetch the user model
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        # Use `get_user_model().objects.create_user` to create a new user
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
+        # Automatically create a token for the new user
         Token.objects.create(user=user)
         return user
-
-"""
-from rest_framework import serializers
-from .models import CustomUser
-from rest_framework_simplejwt.tokens import RefreshToken
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
-
-class TokenSerializer(serializers.Serializer):
-    token = serializers.CharField()
-"""
