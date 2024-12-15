d3e4b7be38acf59b69aa5f4ada525ddc6e900607
Omar Dakane Muhumed <omar@Omar.awsaalah451@gmail.com>
2024-12-15 18:30:56 +0300

    commited in 3 task.

---
+++ social_media_api/posts/notifications/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    actor = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='actor', on_delete=models.CASCADE)
+    verb = models.CharField(max_length=100)  # E.g., "liked your post"
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    target_object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('target_content_type', 'target_object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Notification for {self.recipient.username} - {self.verb}"
