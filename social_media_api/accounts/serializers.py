10e229e6b7c40e499843eebf2989bb5bb6a43009
Omar Dakane Muhumed <omar@Omar.awsaalah451@gmail.com>
2024-12-15 16:56:19 +0300

    Project Setup and User Authentication for a Social Media API.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,49 +1,25 @@
+
+# ...............................
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 from .models import CustomUser
 
-class UserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
-
-lf, data):
-        """
-        Ensure the passwords match during validation.
-        """
-        if data.get('password') != data.get('confirm_password'):
-            raise serializers.ValidationError({"password": "Passwords do not match."})
-        return data
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        """
-        Use create_user method to ensure the password is hashed.
-        """
-        validated_data.pop('confirm_password')  # Remove confirm_password before creating the user
-        return User.objects.create_user(**validated_data)
-
-from rest_framework import serializers
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
-
-# Get the user model
-User = get_user_model()
-
-class RegisterUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
-    confirm_password = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'confirm_password', 'bio', 'profile_picture']
-
-    def validate(self, data):
-        if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError({"password": "Passwords do not match."})
-        return data
-
-    def create(self, validated_data):
-        validated_data.pop('confirm_password')
-        user = get_user_model().objects.create_user(**validated_data)
-        Token.objects.create(user=user)  # Ensure token creation here
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
