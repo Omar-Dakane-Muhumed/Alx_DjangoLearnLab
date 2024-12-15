f219526228af151ba30e8f477cc77360cd2082f8
Omar Dakane Muhumed <omar@Omar.awsaalah451@gmail.com>
2024-12-15 17:38:57 +0300

    1. Implementing Posts and Comments Functionality.

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -21,3 +21,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('posts.urls')),  # Include the posts app URLs under 'api/'
+]
